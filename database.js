5a57e2f6879b5c24c44dccdb11e028942c647d34
bittercounter90 <andromaxac33@gmail.com>
2026-09-06 11:13:14 +0700

    updatedatabase

--- database.js
+++ database.js
@@ -1,1 +1,8 @@
-
+// database.js
+const guestDatabase = {
+    "1": "RucusLiveID",
+    "2": "Budi Santoso",
+    "3": "Siti Aminah",
+    "vip1": "Bapak Kepala Sekolah",
+    // Tambahkan ID unik dan nama tamu di sini
+};
